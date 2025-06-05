caeb77b07ab59c53e5a883330a65c81f95d1c840
Quackzelot <208947166+Quackzelot@users.noreply.github.com>
2025-06-05 12:27:45 +0200

    Add hover

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,6 +2,14 @@ const container = document.querySelector("#container");
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
     const div = document.createElement("div");
+    div.classList.add("sketch");
     container.appendChild(div);
   }
 }
+
+const sketches = document.querySelectorAll(".sketch");
+sketches.forEach(sketch => {
+    sketch.addEventListener("mouseenter", () => {
+        sketch.style = "background-color:lime";
+    });
+});
